d720815b418ccb1a86ee10400cd237eb8ca4e889
Rob Porter <roporter@users.noreply.github.com>
2015-05-19 19:34:23 +0100

    Update centosbaseconfig.sh

--- centosbaseconfig.sh
+++ centosbaseconfig.sh
@@ -6,14 +6,14 @@ if [ -f /etc/centos-release ] ; then
 fi
 
 if [ $DistroBasedOn == "CentOS" ]; then
-     echo "CentOS - Operating System found"
-     echo "Running update"
+     echo "**CentOS - Operating System found"
+     echo "**Running update"
      sudo yum update -y
-     echo "Running Upgrade"
+     echo "**Running Upgrade"
      sudo yum upgrade -y
      FILE=~/.bash_profile
      cp $FILE /tmp
-     echo "Setting Proxy"
+     echo "**Setting Proxy"
      cat << EOF > $FILE
 # .bash_profile
 # Get the aliases and functions
@@ -32,10 +32,10 @@ export ftp_proxy=http://proxy-wsa.esl.cisco.com:80
 export FTP_PROXY=http://proxy-wsa.esl.cisco.com:80
 export no_proxy=localhost,10.52.*,172.16.*,192.168.*
 EOF
-     echo "Disabling SELinux"
-     echo "Making backup to /tmp/"
+     echo "**Making backup to /tmp/"
      FILE=/etc/sysconfig/selinux
      cp $FILE /tmp
+     echo "**Disabling SELinux"
      cat << EOF > $FILE
 # This file controls the state of SELinux on the system.
 # SELINUX= can take one of these three values:
@@ -48,30 +48,30 @@ SELINUX=disabled
 # strict - Full SELinux protection.
 SELINUXTYPE=targeted
 EOF
-     echo "Stopping Network Manager"
+     echo "**Stopping Network Manager"
      service NetworkManager stop
-     echo "Disabling Network Manager on reboot"
+     echo "**Disabling Network Manager on reboot"
      chkconfig NetworkManager off
-     echo "Enabling default network controller"
+     echo "**Enabling default network controller"
      chkconfig network on
-     echo "Starting default network controller"
+     echo "**Starting default network controller"
      service network start
-     echo "Adding EPEL repository"
+     echo "**Adding EPEL repository"
      sudo yum install epel-release -y
-     echo "Enabling new repositories"
+     echo "**Enabling new repositories"
      sed '/\[epel\]/,/enabled=0/ { s/enabled=0/enabled=1/ }' /etc/yum.repos.d/epel.repo -i
      sed '/\[epel-source\]/,/enabled=0/ { s/enabled=0/enabled=1/ }' /etc/yum.repos.d/epel.repo -i
      sed '/\[epel-debuginfo\]/,/enabled=0/ { s/enabled=0/enabled=1/ }' /etc/yum.repos.d/epel.repo -i
-     echo "Setting YUM Proxy"
+     echo "**Setting YUM Proxy"
      sed '/\[main\]/,/proxy/ { s@proxy=.*@proxy=http://proxy-wsa.esl.cisco.com:80@ }' /etc/yum.conf -i
-     echo "Updating packages"
+     echo "**Updating packages"
      sudo yum update
-     echo "Installing some base tools"
+     echo "**Installing some base tools"
      sudo yum install nano wget
-     echo "Installing RDO Juno"
+     echo "**Installing RDO Juno"
      sudo yum install -y https://repos.fedorapeople.org/repos/openstack/openstack-juno/rdo-release-juno-1.noarch.rpm
-     echo "Installing packstack"
+     echo "**Installing packstack"
      sudo yum install -y openstack-packstack
-     echo "Running packstack"
+     echo "**Running packstack"
      packstack --allinone
 fi
