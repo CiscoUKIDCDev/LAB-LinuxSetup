a45604bd2d4804a3e3a2e989c6273e4f941420f1
Rob Porter <roporter@users.noreply.github.com>
2015-05-19 14:16:29 +0100

    Update centosbaseconfig.sh

--- centosbaseconfig.sh
+++ centosbaseconfig.sh
@@ -1,10 +1,2 @@
-if [ -f /etc/centos-release ] ; then
-    DistroBasedOn='CentOS'
-    DIST=`cat /etc/redhat-release |sed s/\ release.*//`
-    PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-    REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
-    
-echo $DistroBasedOn
-echo $DIST
-echo $PSUEDONAME
-echo $REV
+yum -y update
+yum -y upgrade
